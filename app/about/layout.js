f9f3123f7024dde39a926bec1565f2a7d01885c2
Ankur Jhavery <ajhavery@gmail.com>
2023-05-13 22:22:48 +0530

    minor changes

---
+++ app/about/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title:
+    "About Dhamika.com - India's leading supplier aggregator and end to end service provider for export-import",
+  description:
+    "Connecting Global buyers with Indian Suppliers of Industrial and Electrical Goods",
+};
+
+export default function AboutLayout({ children }) {
+  return <section>{children}</section>;
+}
---
+++ app/about/page.js
@@ -0,0 +1,43 @@
+function AboutUs() {
+  return (
+    <div>
+      <div className="container mx-auto px-4 py-8">
+        <h2 className="text-3xl font-bold mb-4">About Us</h2>
+        <p className="text-lg text-gray-600 mb-8">
+          Welcome to Your Company Name, a B2B e-commerce platform connecting
+          suppliers of electrical and mechanical products from India to global
+          buyers. We aim to simplify and streamline the process of sourcing
+          high-quality products from trusted suppliers in India.
+        </p>
+        <h2 className="text-xl font-bold mb-4">Our Mission</h2>
+        <p className="text-lg text-gray-600 mb-8">
+          Our mission is to empower businesses worldwide by providing a
+          user-friendly platform that facilitates efficient trade between
+          suppliers and buyers. We strive to offer a comprehensive selection of
+          electrical and mechanical products from reliable Indian suppliers,
+          while ensuring seamless communication, secure transactions, and
+          excellent customer service.
+        </p>
+        <h2 className="text-xl font-bold mb-4">Why Choose Us?</h2>
+        <ul className="list-disc list-inside text-lg text-gray-600 mb-8">
+          <li>Wide range of high-quality electrical and mechanical products</li>
+          <li>Verified and trusted suppliers from India</li>
+          <li>Competitive prices and flexible payment options</li>
+          <li>Secure and transparent transaction process</li>
+          <li>Efficient communication and customer support</li>
+        </ul>
+        <h2 className="text-xl font-bold mb-4">Contact Us</h2>
+        <p className="text-lg text-gray-600 mb-4">
+          We are always here to assist you. If you have any questions,
+          suggestions, or inquiries, please feel free to reach out to our
+          customer support team:
+        </p>
+        <p className="text-lg text-gray-600">
+          Email: support@yourcompanyname.com
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
